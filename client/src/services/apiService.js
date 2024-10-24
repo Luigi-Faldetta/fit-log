170b4217877576343edefbcd1edd8886b3d237ae
Luigi-Faldetta <luigifaldetta@gmail.com>
2024-10-24 11:42:33 +0200

    feat: updated API URL with Render URL

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,6 +1,6 @@
 // src/services/apiService.js
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getWorkouts = async () => {
   try {
