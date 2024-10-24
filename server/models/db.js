4dbcd40bcd16f8644812191f56a0a64a00a38228
Luigi-Faldetta <luigifaldetta@gmail.com>
2024-10-24 10:53:15 +0200

    feat: update db connection url for deployent

--- server/models/db.js
+++ server/models/db.js
@@ -1,12 +1,25 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('workouts_db', 'postgres', 'ffjjkl', {
-  host: 'localhost',
-  dialect: 'postgres',
-  define: {
-    noPrimaryKey: true,
-  },
-  logging: false,
-});
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'workouts_db',
+  'postgres',
+  'ffjjkl',
+  {
+    host: process.env.DATABASE_URL ? undefined : 'localhost',
+    dialect: 'postgres',
+    define: {
+      noPrimaryKey: true,
+    },
+    logging: false,
+    dialectOptions: {
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { require: true, rejectUnauthorized: false }
+          : false,
+    },
+  }
+);
 
 module.exports = {
   sequelize,
