27d747e59881827c1615aab53227c252a9feb575
Luigi-Faldetta <luigifaldetta@gmail.com>
2025-02-16 19:43:52 +0100

    feat: apiService file updated

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,6 +1,8 @@
 // src/services/apiService.js
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL.replace(/\/$/, '');
+const API_BASE_URL = (
+  process.env.VITE_API_BASE_URL || 'http://localhost'
+).replace(/\/$/, '');
 
 export const getWorkouts = async () => {
   try {
